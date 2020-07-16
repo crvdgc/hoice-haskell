b4da3756dd75e911427ad319462206ff7d35195d
crvdgc <yokis1997@pku.edu.cn>
2020-07-17 01:29:54 +0900

    index chc

--- src/CHC.hs
+++ src/CHC.hs
@@ -1,17 +1,72 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RecordWildCards #-}
 module CHC where
 
 import           Control.Monad
-import           Control.Monad.ST
-import           Data.Array
-import           Data.Array.ST
-import           Data.IntMap
-import qualified Data.IntSet          as S
-import qualified Data.List.NonEmpty   as NE
-import           Data.STRef
-import qualified Data.Text            as T
-import qualified Data.Text.Read       as TR
+import qualified Data.IntMap            as M
+import qualified Data.Set               as S
+import qualified Data.Text              as T
+import qualified Language.Assertion.LIA as L
 import           Language.SMT2.Syntax
+
+data FuncApp v f = FuncApp { func :: f        -- ^ function
+                           , vars :: S.Set v  -- ^ arguments
+                           }
+  deriving (Eq, Show)
+
+instance Functor (FuncApp v) where
+  fmap f funcApp@FuncApp{..} = funcApp { func = f func }
+
+data Clause v f = Clause { quals :: S.Set v        -- ^ @forall@ qualified variables
+                         , body  :: [FuncApp v f]  -- ^ uninterpreted preds
+                         , phi   :: L.LIA Bool v   -- ^ constraints
+                         , heads :: [FuncApp v f]  -- ^ uninterpreted preds
+                         }
+  deriving (Eq, Show)
+
+instance Functor (Clause v) where
+  fmap f cls@Clause{..} = cls { body = (fmap . fmap) f body, heads = (fmap . fmap) f heads }
+
+newtype CHC v f = CHC [Clause v f]
+
+dispatchPreds :: ([FuncApp v f] -> a) -> ([FuncApp v f] -> b) -> (a -> b -> c) -> Clause v f -> c
+dispatchPreds fHeads fBody g cls = g (fHeads $ heads cls) (fBody $ body cls)
+
+bothPreds :: ([FuncApp v f] -> a) -> (a -> a -> b) -> Clause v f -> b
+bothPreds = join dispatchPreds
+
+setToMap :: S.Set a -> M.IntMap a
+setToMap = M.fromAscList . zip [0..] . S.toAscList
+
+funcs :: (Ord f) => Clause v f -> S.Set f
+funcs = bothPreds getFuncs S.union
+  where
+    getFuncs = S.fromList . fmap func
+
+allVars :: (Ord v) => Clause v f -> S.Set v
+allVars cls = S.unions . fmap (\f -> f cls) $ [quals, bothPreds getVars S.union, L.freeVarsLIA . phi]
+  where
+    getVars = S.unions . fmap vars
+
+indexCHCFunc :: (Ord f) => CHC v f -> (CHC v Int, M.IntMap f)
+indexCHCFunc (CHC clss) = (CHC indexed, ixs)
+  where
+    allFuncs = S.unions . fmap funcs $ clss
+    indexed = (fmap . fmap) (`S.findIndex` allFuncs) clss
+    ixs = setToMap allFuncs
+
+
+indexClauseVars :: (Ord v) => Clause v f -> (Clause Int f, M.IntMap v)
+indexClauseVars cls@Clause{..} = (indexed, ixs)
+  where
+    avs = allVars cls
+    ixs = setToMap avs
+    find = flip S.findIndex avs
+    findVars funcApp@FuncApp{..} = funcApp { vars = S.map find vars }
+    indexed = Clause { quals = S.map find quals
+                     , body  = findVars <$> body
+                     , phi   = find <$> phi
+                     , heads = findVars <$> heads
+                     }
 
 -- | parse a script to a graph, only accept the following commands:
 --
